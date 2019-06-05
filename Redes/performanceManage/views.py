93b31dfcbdbcd4a72c51a8bd7ed2c5550615e683
Jesus <jesus-antonio-29@hotmail.com>
2019-06-05 09:49:53 -0500

    Busqueda por nombre en performance

--- Redes/performanceManage/views.py
+++ Redes/performanceManage/views.py
@@ -14,6 +14,15 @@ import pytz
 
 # Create your views here.
 
+host = "50.0.0.2"
+routers = {'R2': ['50.0.0.1', '10.0.80.2', '10.0.80.5'],
+           'R1': ['10.0.80.1', '10.0.80.13'],
+           'R3': ['10.0.80.6', '10.0.80.14', '10.0.80.9'],
+           'R4': ['10.0.80.10', '172.16.200.1'],
+           'R5': ['172.16.200.2', '172.16.200.5', '172.16.200.10'],
+           'R6': ['172.16.200.9', '172.16.200.14'],
+           'R7': ['172.16.200.13'],
+           'R8': ['172.16.200.6']}
 
 class HomeView(View):
     def get(self, request, *args, **kargs):
@@ -27,23 +36,30 @@ def nueva_estadistica(request, fecha, ip, tipo, valor):
     e = Estadistica(fecha=fe, ip=ip, tipo=tipo, valor=valor)
     e.save()
     if tipo == 'CPU' and int(valor) >= 80:
-        requests.get("http://127.0.0.1:8000/configManage/reportes/" + ip +
+        requests.get("http://" + host + ":8000/configManage/reportes/" + ip +
                      "/Uso de CPU crítico." + "/" + time.strftime("%c"))
     elif tipo == 'CPU' and 50 <= int(valor) < 80:
-        requests.get("http://127.0.0.1:8000/configManage/reportes/" + ip +
+        requests.get("http://" + host + ":8000/configManage/reportes/" + ip +
                      "/Uso de CPU moderado." + "/" + time.strftime("%c"))
 
     if tipo == 'Memory' and int(valor) >= 80:
-        requests.get("http://127.0.0.1:8000/configManage/reportes/" + ip +
+        requests.get("http://" + host + ":8000/configManage/reportes/" + ip +
                      "/Uso de memoria crítico." + "/" + time.strftime("%c"))
     elif tipo == 'Memory' and 60 <= int(valor) < 80:
-        requests.get("http://127.0.0.1:8000/configManage/reportes/" + ip +
+        requests.get("http://" + host + "/configManage/reportes/" + ip +
                      "/Uso de memoria moderado." + "/" + time.strftime("%c"))
 
 
 def mostrarPerformance(request):
     # obtener parametros del POST
-    ip = request.POST["ip_source"]
+    router = request.POST["ip_source"]
+    ip = ""
+    print(router)
+    try:
+        ip = routers[router][0]
+    except:
+        return render(request, 'estadisticas.html')
+
     fechaini = datetime.strptime(request.POST["fecha_ini"], "%Y-%m-%dT%H:%M")
     fechafin = datetime.strptime(request.POST["fecha_fin"], "%Y-%m-%dT%H:%M")
     fechaini = pytz.UTC.localize(fechaini)
@@ -115,9 +131,16 @@ def mostrarPerformance(request):
 def mostrar10Links(request):
     estadisticas = Estadistica.objects.filter(tipo='Interface-in')
     interfaces = {}
+    dispositivo = ""
     for estadistica in estadisticas:
         interfaz, x = str(estadistica.valor).split(':')
-        interfaz = estadistica.ip + ':' + interfaz
+        for r in routers.items():
+            print(r[1])
+            if r[1].__contains__(estadistica.ip):
+                dispositivo = r[0]
+                print(dispositivo)
+                break
+        interfaz = dispositivo + ': ' + interfaz
         if interfaces.get(interfaz) is None:
             interfaces[interfaz] = 0
         interfaces[interfaz] = interfaces[interfaz] + int(x)
